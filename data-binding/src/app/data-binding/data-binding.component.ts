59f8d09a4f611cb211e67c47aec1d1295ebcd44f
rlins <rlins@tecgraf.puc-rio.br>
2019-03-29 19:58:29 -0300

    Projeto data biding

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+
+  url:string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImage:string = 'http://lorempixel.com/400/200/sports'
+  valorAtual: string = ""
+  valorSalvo: string = ""
+  isMouseOver: boolean = false;
+  isMouseOut: boolean = true;
+
+  constructor() { }
+
+  getValor(){
+    return 1+1;
+  }
+
+  getCurtiCurso(){
+    return true;
+  }
+
+
+  botaoClicado(){
+    alert('Botao clicado');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor: any){
+    this.valorSalvo = valor;
+  }
+
+  onMouseoverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  ngOnInit() {
+  }
+
+}
